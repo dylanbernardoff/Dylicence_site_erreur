7f2d573325d4d51131bf947bbd31c45c4a910613
Dylan <36641401+dylanbernardoff@users.noreply.github.com>
2023-08-10 00:47:55 +0200

    Structure base

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>ERREUR LICENCE</title>
+	<link rel="shortcut icon" href="./assets/icon.png" type="image/svg">
+	<link rel="stylesheet" href="./assets/bootstrap.min.css">
+	<link rel="stylesheet" href="./assets/ud-styles.css">
+	<link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.2.1/css/all.css" />
+</head>
+<body style="background-color: #1a1a1a;">
+	<section class="ud-404">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="ud-404-wrapper" style="background-color: #252525;">
+						<div class="ud-404-content">
+							<div style="text-align: center;">
+								<i class="fa-regular fa-cloud-exclamation fa-beat-fade" style="color: #e91e63;font-size: 67px;margin-bottom: 38px;"></i>
+								<h2 class="ud-404-title" style="color: #e91e63;">ERREUR DE LICENCE</h2>
+								<h5 class="ud-404-subtitle" style="color: #fff;">Votre licence a <b>expiré</b> ou a été <b>annuler</b> par un administrateur.<br>En cas de problème merci de contacter le service client.</h5>
+							</div>
+							<!-- <ul class="ud-404-links" style="display: block;justify-content: flex-start;text-align: center;">
+								<li>
+									<a href='#' style='color:#ffff;'>Contactez le service client</a> 
+								</li><br>
+							</ul> -->
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</body>
+</html>
